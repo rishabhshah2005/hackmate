1a8cf7a3b1a8e489aa96f51d09b3e0fdea974f87
Rishabh <rishabhshah2005@gmail.com>
2025-08-14 16:20:46 +0530

    added teams

--- hackmate_backend/teams/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('create/', create_team, name='createteam'),
-    path("get_hackathons/", get_hackathons, name='get_hackathons'),
-]
